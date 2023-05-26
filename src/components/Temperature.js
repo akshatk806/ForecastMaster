3f47f85f92e73747ea6fb0fa91ee4444dce2a267
akshatk806 <akshatk806@gmail.com>
2023-05-27 00:46:59 +0530

    adding the temperature items to the DOM

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import WeatherCard from './WeatherCard';
 import "./style.css"
 
 const Temperature = () => {
@@ -11,15 +12,15 @@ const Temperature = () => {
 
     const getWeatherInfo = async ()=>{
         try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=0fced3f07d371271b167d454426a8eaa&units=metric`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=0fced3f07d371271b167d454426a8ead&units=metric`;
             const response = await fetch(url);
             const data = await response.json();
-            // console.log(data);
+            // console.log(data.weather[0].main);
 
             // object destruturing
             const {temp, humidity, pressure} = data.main;           // temperature in celcius from API
 
-            const {main : weathermood} = data.weather[0].main;  // for weather mood (clouds and all)
+            const {main:weathermood} = data.weather[0];  // for weather mood (clouds and all)
 
             const {name} = data;
 
@@ -47,7 +48,6 @@ const Temperature = () => {
         }
         
     }
-
     useEffect(()=>{
         getWeatherInfo();
     },[]);       // passing the empty array it means ki ab me sirf page ko refresh karu first time to hi useEffect ke andar jo h wo run ho else na ho
@@ -62,68 +62,8 @@ const Temperature = () => {
             </div>
         </div>
 
-        {/* container of weather details, temperature card */}
-        <article className="widget">
-            <div className="weatherIcon">
-                <i className={"wi wi-day-sunny"}></i>
-            </div>
-
-            <div className="weatherInfo">
-                <div className="temperature">
-                    <span>25.5&deg;</span>
-                </div>
-                <div className="description">
-                    <div className="weatherCondition">
-                        sunny
-                    </div>
-                    <div className="place">
-                        Delhi, India
-                    </div>
-                </div>
-            </div>
-            <div className="date">
-                {new Date().toLocaleString()}
-            </div>
-
-
-            {/* our 4 columns section */}
-            <div className="extra-temp">
-                <div className="temp-info-minmax">
-                    <div className="two-sided-section">
-                        <p><i className={"wi wi-sunset"}></i></p>
-                        <p className="extra-info-leftside">
-                            19.19 PM <br />
-                            Sunset
-                        </p>
-                    </div>
-
-                    <div className="two-sided-section">
-                        <p><i className={"wi wi-humidity"}></i></p>
-                        <p className="extra-info-leftside">
-                            19.19 PM <br />
-                            Humidity 
-                        </p>
-                    </div>
-                </div>
-
-                <div className="weather-extra-info">
-                    <div className="two-sided-section">
-                        <p><i className={"wi wi-rain"}></i></p>
-                        <p className="extra-info-leftside">
-                            19.19 PM <br />
-                            Pressure 
-                        </p>
-                    </div>
-                    <div className="two-sided-section">
-                        <p><i className={"wi wi-strong-wind"}></i></p>
-                        <p className="extra-info-leftside">
-                            19.19 PM <br />
-                            Speed 
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </article>
+        {/* passing the props to weather card*/}
+        <WeatherCard {...temperatureInfo}/>
     </>
   )
 }
